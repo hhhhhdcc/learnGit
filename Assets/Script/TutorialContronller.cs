a324b7d9d4a3fa58e0e591e0b1d275849ea0997f
Tuan Pham <pphamtuan1811@gmail.com>
2025-11-04 19:53:41 +0700

    sua vi tri

--- Assets/Script/TutorialContronller.cs
+++ Assets/Script/TutorialContronller.cs
@@ -7,9 +7,8 @@ public class TutorialContronller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);  
+        transform.position += new Vector3(100,0,0);  
     }
-
     // Update is called once per frame
     void Update()
     {
